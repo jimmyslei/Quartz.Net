2c77e795037422b4db9262430dc0965f45439075
jimmy <787478300@qq.com>
2025-10-23 18:25:10 +0800

    init

---
+++ src/Jim.Quartz/Extensions/QuartzExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#if !NETSTANDARD2_0
+namespace Jim.Quartz;
+#else
+namespace Jim.Quartz {
+#endif
+public static class QuartzExtensions
+{
+    /// <summary>
+    /// 注册redis服务
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddRedis(this IServiceCollection services)
+    {
+       
+        return services;
+    }
+}
+#if NETSTANDARD2_0
+}
+#endif
