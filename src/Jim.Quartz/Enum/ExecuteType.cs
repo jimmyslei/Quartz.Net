41196a479da0bd1057909d4800f034f1c53f8b5a
jimmy <787478300@qq.com>
2025-10-24 17:42:14 +0800

    增加model参数和enum定时类型

---
+++ src/Jim.Quartz/Enum/ExecuteType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jim.Quartz
+{
+    public enum ExecuteType
+    {
+        /// <summary>
+        /// 执行一次
+        /// </summary>
+        Once,
+        /// <summary>
+        /// 重复执行
+        /// </summary>
+        Repeat,
+        /// <summary>
+        /// cron表达式执行
+        /// </summary>
+        Cron,
+
+    }
+}
---
+++ src/Jim.Quartz/Models/ExecuteModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jim.Quartz
+{
+    public class ExecuteModel
+    {
+        /// <summary>
+        /// 任务Id
+        /// </summary>
+        public string ScheduleId { get; set; }
+        /// <summary>
+        /// 任务开始时间(秒)
+        /// </summary>
+        public double StartAt { get; set; }
+        /// <summary>
+        /// 执行周期(秒)
+        /// </summary>
+        public double Time { get; set; }
+        /// <summary>
+        /// 任务执行类型
+        /// </summary>
+        public ExecuteType ExecuteType { get; set; }
+        /// <summary>
+        /// Cron表达式
+        /// 当ExecuteType为Cron时，必须填写此项
+        /// </summary>
+        public string Cron { get; set; }
+        /// <summary>
+        /// 需要执行的函数
+        /// </summary>
+        public FunctionDelegate Function { get; set; }
+
+        //public Func<Task> Function { get; set; }
+
+    }
+}
